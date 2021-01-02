fa315c30077b290c85b5ecc6da1ad57072cacb41
Xoliviaxx <lili.poblocka@outlook.com>
2021-01-02 18:23:28 +0100

    dodany dwukropek

--- baza.py
+++ baza.py
@@ -2,4 +2,4 @@ print("Liczymy iloczyny.")
 Numer1 = int(input("Podaj pierwszą liczbę "))
 Numer2 = int(input('Podaj drugą liczbę '))
 score = Numer1*Numer2
-print(f"Iloczyn liczb to {score}")
+print(f"Iloczyn liczb to: {score}")
